d891dfab7b71414ffc00984b25ec343fc2a87c4a
Project Team <project.teamedit@gmail.com>
2025-09-23 08:45:31 +0000

    ini kenapa masih gak bisa padahal gw udah bener kan masukin variablenya

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: process.env.Gemini_key,
     }),
   ],
   model: 'googleai/gemini-1.5-flash-latest',
