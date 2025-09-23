17b0e405fcadd5d8d806ce6a522aac7485b8a37f
Project Team <project.teamedit@gmail.com>
2025-09-23 07:10:28 +0000

    design layar dan chat nya perbarui dong terus font nya pake yang keren

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight: ["400", "600"] 
+});
 
 export const metadata: Metadata = {
-  title: "Chatbot",
+  title: "Chat.Maskuh",
   description: "A friendly chatbot to assist you.",
 };
 
@@ -17,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id" suppressHydrationWarning>
-      <body className={`${inter.className} bg-gray-100 dark:bg-gray-900 transition-colors duration-300`}>
+      <body className={`${poppins.className} bg-gray-100 dark:bg-gray-900 transition-colors duration-300`}>
         {children}
         <Toaster />
       </body>
