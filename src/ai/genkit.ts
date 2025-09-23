4f2020ac9ae21616000429e75a3a33caa20b50c5
Project Team <project.teamedit@gmail.com>
2025-09-23 03:32:09 +0000

    File changes

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,8 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
 import {openAI} from 'genkitx-openai';
 
 export const ai = genkit({
   plugins: [
-    googleAI(),
     openAI({
       apiKey: process.env.OPENAI_API_KEY,
     }),
